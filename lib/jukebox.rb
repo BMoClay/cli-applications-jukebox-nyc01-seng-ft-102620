1ea665df5675cedd0d1a60b7bb7dbf15ef2807bc
Ben Morgan-Cleveland <benmorgancleveland@gmail.com>
2020-10-20 14:02:49 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -21,32 +21,40 @@ puts "- exit : exits this program"
 end
 
 def list(songs)
-  index = 0
-    while index < songs.length do
-    songs.each_with_index{|song, index|}
-    puts "{index + 1}. #{song}"
-    end
-  index += 1
+  songs.each_with_index do |song, index|
+    puts "#{index +1}. #{song}"
+  end
 end
 
 def play(songs)
   puts "Please enter a song name or number:"
   user_input = gets.strip
-  #valid_number = ((0...9) + 1).to_s
-  #valid_number = user_input.to_i.between?(1,9)
-  #valid_number = user_input.to_i >= 1 && user_input.to_i <= 9
-  valid_number = user_input.to_i(1...9)
-  binding.pry
-  valid_song = songs.one?(songs[0,8])
-    if user_input == valid_number
+    if user_input.to_i.between?(1,9)
       puts "Playing #{songs[user_input.to_i - 1]}"
-      binding.pry
-    else user_input == valid_song
+    elsif songs.include? user_input
       puts "Playing #{user_input}"
-    end
+    else
     puts "Invalid input, please try again"
   end
+end
 
 def exit_jukebox
   puts "Goodbye"
+end
+
+def run(songs)
+  user_input = gets.strip
+    if user_input == "help"
+      help
+    elsif user_input == "list"
+      list(songs)
+    elsif user_input == "play"
+      play(songs)
+    else user_input == "exit"
+      exit_jukebox
+    end
+  end
+
+
+
 end
